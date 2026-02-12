e0098daf20a14a10b054249dc8fdcfd00b8ce05d
Chaozen <sven.holzmann58@gmail.com>
2026-02-12 12:46:54 +0100

    - Update files   - preislogik.py (Ticket Preise und Mehrfachtickets (Berechnung ist gleich geblieben)   - ubahn.py (korrigiert Rundungsproblematik aus Sprint 2 testcase 3)

--- Sprint 3. Reiseinformationen und Tariflogik/preislogik.py
+++ Sprint 3. Reiseinformationen und Tariflogik/preislogik.py
@@ -1,12 +1,17 @@
 class PreisLogik:
-    PREISE = {
+    PREISE_EINZEL = {
         "Kurzstrecke": 1.50,
         "Mittelstrecke": 2.00,
+        "Langstrecke": 3.00
+    }
+    PREISE_MEHRFAHRT = {
+        "Kurzstrecke": 5.00,
+        "Mittelstrecke": 7.00,
         "Langstrecke": 10.00
     }
 
     def __init__(self):
-        pass  # keine Liste mehr nötig
+        pass
 
     def ticket_typ(self, start, ziel, stationen):
         indices = {s.name: i for i, s in enumerate(stationen)}
@@ -16,26 +21,32 @@ class PreisLogik:
 
         distanz = abs(indices[ziel] - indices[start])
 
+
         if distanz <= 3:
             return "Kurzstrecke"
-        elif distanz <= 6:
+        elif distanz <= 8:
             return "Mittelstrecke"
         else:
             return "Langstrecke"
 
     def berechne_preis(self, ticket_typ, ermaessigung="Nein", barzahlung="Nein", einzelfahrt="Nein"):
-        """Berechnet den endgültigen Preis für die gewählte Kombination"""
-        grundpreis = self.PREISE[ticket_typ]
+        """Berechnet den endgültigen Preis basierend auf der Wahl"""
 
-        rabatt_prozent = 0
-        aufpreis_prozent = 0
+        # Wahl der Preisliste
+        if einzelfahrt == "Ja":
+            grundpreis = self.PREISE_EINZEL[ticket_typ]
+        else:
+            grundpreis = self.PREISE_MEHRFAHRT[ticket_typ]
+
+        prozent_summe = 0
 
         if ermaessigung == "Ja":
-            rabatt_prozent += 20
+            prozent_summe -= 20
         if barzahlung == "Ja":
-            aufpreis_prozent += 10
+            prozent_summe += 15
         if einzelfahrt == "Ja":
-            aufpreis_prozent += 5
+            prozent_summe += 10
 
-        endpreis = grundpreis * (1 - rabatt_prozent / 100) * (1 + aufpreis_prozent / 100)
+        # Anwendung der Summe auf den Grundpreis
+        endpreis = grundpreis * (1 + prozent_summe / 100)
         return round(endpreis, 2)
